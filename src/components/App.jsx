7fa5e95ae25deb19fe4b8948fc5a297ad6b75dd6
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-07-15 22:56:30 +0100

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,12 @@
-import friends from "./data/friends.json";
 import FriendList from "../components/FriendList";
+import friends from "./data/friends.json";
+
+import Profile from "../components/Profile";
+import user from "./data/user.json";
+
+import Statistics from "./Statistics";
+import data from '../components/data/data.json';
+
 import { Fragment } from "react";
 
 export default function App() {
@@ -13,6 +20,22 @@ export default function App() {
           key={friend.id}
         />
       ))}
+        <Profile
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+
+      />
+
+      <Statistics
+       title="Upload stats" stats={data}
+       statsD={data}
+      
+      />
+
+
     </Fragment>
   );
 };
